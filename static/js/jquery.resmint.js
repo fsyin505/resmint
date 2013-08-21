b2998ea8dae4645c4c931b8420f3908dbde461c2
Frank Yin <fsyin505@yahoo.com>
2013-08-21 21:57:15 +0800

    updated attribution

--- static/js/jquery.resmint.js
+++ static/js/jquery.resmint.js
@@ -2,12 +2,10 @@
 
 SMINT V1.0 by Robert McCracken
 SMINT V2.0 by robert McCracken with some awesome help from Ryan Clarke (@clarkieryan) and mcpacosy ‏(@mcpacosy)
-
-SMINT is my first dabble into jQuery plugins!
-
 http://www.outyear.co.uk/smint/
 
-If you like Smint, or have suggestions on how it could be improved, send me a tweet @rabmyself
+RESMINT V1.0 - A Responsive Smint by Frank Yin
+frank@warewoof.com
 
 */
 
